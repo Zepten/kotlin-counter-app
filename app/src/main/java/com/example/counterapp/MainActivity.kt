d4f6ef9882e9e3ef7e90a9b6c505a0b19a53a6fc
Zepten <39930495+Zepten@users.noreply.github.com>
2022-09-14 05:43:17 +0300

    added step input validation

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -7,19 +7,12 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Поле со значением счётчика
-        val valueText: TextView = findViewById(R.id.valueTextView)
-        valueText.text = currentValueText
-
-        // Поле ввода шага счётчика
-        val stepText: EditText = findViewById(R.id.stepEditText)
-        stepText.setText(resources.getText(R.string.default_step_text))
 
         // Кнопка "прибавить"
         val plusButton: ImageButton = findViewById(R.id.plusImageButton)
@@ -29,12 +22,46 @@ class MainActivity : AppCompatActivity() {
         val minusButton: ImageButton = findViewById(R.id.minusImageButton)
         minusButton.setOnClickListener { updateCounterValue(add=false) }
 
+        // Поле со значением счётчика
+        val valueText: TextView = findViewById(R.id.valueTextView)
+        valueText.text = currentValueText
+
+        // Поле ввода шага счётчика
+        val stepText: EditText = findViewById(R.id.stepEditText)
+        stepText.setText(resources.getText(R.string.default_step_text))
+        stepText.doOnTextChanged { _, _, _, _ ->
+            if (isValidValueText()){
+                plusButton.isEnabled = true
+                minusButton.isEnabled = true
+            } else {
+                plusButton.isEnabled = false
+                minusButton.isEnabled = false
+            }
+        }
+
         // Кнопка "перейти на 2-ю активность"
         val secondActivityButton: Button = findViewById(R.id.secondActivityButton)
         secondActivityButton.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             intent.putExtra("value", valueText.text.toString())
             startActivity(intent)
+        }
+    }
+
+    private fun isValidValueText(): Boolean {
+        val stepText: EditText = findViewById(R.id.stepEditText)
+        return when {
+            stepText.text.isBlank() -> {
+                stepText.error = resources.getText(R.string.step_blank_error)
+                false
+            }
+            stepText.text.contains(regex = Regex("[^0-9]")) -> {
+                stepText.error = resources.getText(R.string.step_not_digits_error)
+                false
+            }
+            else -> {
+                true
+            }
         }
     }
 
